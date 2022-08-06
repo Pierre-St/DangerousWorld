dbff68736e56e105dab5ff9d18e3e7c423eb8ba0
Pierre Stirnweiss <pstirnweiss@googlemail.com>
2022-08-06 13:02:27 +0200

    rebase

---
+++ DangerousWorld - Copy/DangerousWorldStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PierreStirnweiss.DangerousWorld
+{
+    static class DangerousWorldStrings
+    {
+        // CrippledEffect
+        public static LocString CRIPPLED_EFFECT_TITLE = "Crippled";
+        public static LocString CRIPPLED_EFFECT_DESC = "This Duplicant has been crippled and can barely move.";
+
+        // DangerousWorldAggroCritters
+        public static LocString AGGRO_CRITTER_AGGRO_TITLE = "Aggressive";
+        public static LocString AGGRO_CRITTER_AGGRO_DESC = "This critter is aggressive. It will go for the throat.";
+
+        public static LocString AGGRO_CRITTER_ATTACK_TITLE = "Attacking";
+        public static LocString AGGRO_CRITTER_ATTACK_DESC = "It has no mercy!";
+
+        public static LocString AGGRO_CRITTER_SUBDUED_TITLE = "Subdued";
+        public static LocString AGGRO_CRITTER_SUBDUED_DESC = "Catching its breath for the next attack.";
+
+        // ProstheticsFabricator
+        public static LocString PROSTHETICS_FABRICATOR_NAME = "Prosthetics Fabricator";
+        public static LocString PROSTHETICS_FABRICATOR_DESC = "Searching through a drawer, a janitor stumbled across a book called \"Prosthetics for dummies: The six million dollar dupe\"\r\nNobody knows what a dollar is, but the schematics inside the book look really promising.";
+        public static LocString PROSTHETICS_FABRICATOR_EFFECT = "This advanced machine can manufacture every dupe's part, better";
+
+        public static LocString PROSTHETICS_SPLINTER_NAME = "Splinter";
+        public static LocString PROSTHETICS_SPLINTER_DESC = "Ensures my Duplicants can remain functionnal with broken limbs.";
+        public static LocString PROSTHETICS_SPLINTER_EFFECT = "Patch-up a dupe with the Crippled effect.\n\nMust be applied at a Triage cot.";
+        public static LocString PROSTHETICS_SPLINTER_RECIPE_DESC = "A splinter for patching-up broken limbs";
+        public static LocString PROSTHETICS_SPLINTER_GENERICNAME = "Prosthesis";
+
+        // Confined
+        //public static LocString CONFINED_TITLE = "Confined";
+        //public static LocString CONFINED_DESC = "These Duplicants are stuck in a small space:";
+
+        // Trapped
+        //public static LocString TRAPPED_TITLE = "Trapped";
+        //public static LocString TRAPPED_DESC = "These Duplicants cannot access vital parts of the colony:";
+    }
+}
